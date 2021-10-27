2a0b5b644aac00341f351351fd29f339783f2efd
ruzzHammer <18russell.1998@gmail.com>
2021-10-27 15:11:32 +0300

    feat: treeview with path display

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,61 @@
 <template>
   <div id="app">
-    <tree-item
-      :item="treeData"
-    ></tree-item>
+    <path-display :path="path"/>
+    <ul class="tree">
+      <tree-item
+        :item="treeData"
+        @path="getPath($event)"
+        currentPath="/"
+        :displayedPath="path"
+        :isFocused="true"
+      ></tree-item>
+    </ul>
   </div>
 </template>
 
 <script>
-import treeData from '../public/static/node_modules.json'
-import treeItem from './components/TreeItem.vue'
+import TreeData from '../public/static/node_modules.json'
+import TreeItem from './components/TreeItem.vue'
+import PathDisplay from './components/PathDisplay.vue'
 export default {
   name: 'App',
+  components: {
+    TreeItem,
+    PathDisplay
+  },
   data() {
     return {
-      treeData: treeData
+      treeData: TreeData,
+      path: '/'
     }
   },
-  components: {
-    treeItem
+  provide() {
+    return {
+      getPath: this.getPath,
+    }
   },
   methods: {
-  },
+    getPath(path) {
+      this.path = path
+    }
+  }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: #003780;
+  margin: 0 auto;
+}
+*, *::before, *::after {
+  box-sizing: border-box;
 }
 .item {
   cursor: pointer;
@@ -37,6 +63,26 @@ export default {
 .bold {
   font-weight: bold;
 }
+.icon {
+  fill: #003780;
+}
+p {
+  margin: 0 0 5px;
+}
+ul, li {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
 
+ul {
+  padding-left: 1em;
+}
+
+.tree {
+  margin: 10px auto;
+  padding: 0 10px;
+  width: fit-content;
+}
 
 </style>
--- src/components/TreeItem.vue
+++ src/components/TreeItem.vue
@@ -1,47 +1,102 @@
 <template>
     <li class="item">
-      <div
-        @click="toggle">
-            {{ item.name }}
-        <span v-if="isFolder">[{{ isOpen ? '-' : '+' }}]</span>
-      </div>
-      <ul v-show="isOpen" v-if="isFolder">
-        <tree-item
-          class="item"
-          v-for="(child, index) in item.contents"
-          :key="index"
-          :item="child"
-        ></tree-item>
-      </ul>
+        <div 
+        @click.stop="handleSelection()"
+        :class="[{
+            selected: displayedPath === itemPath,
+        },
+        'item__main']">
+            <component 
+            :is="icon"
+            v-bind="{active: isOpen}"
+            ></component>
+            <p>{{ item.name }}</p>
+        </div>
+        <div class="item__content">
+            <ul v-if="isFolder && isOpen">
+                <tree-item
+                v-for="(child, index) in item.contents"
+                @path="getPath($event)"
+                :key="index"
+                :item="child"
+                :currentPath="itemPath"
+                :displayedPath="displayedPath"
+                ></tree-item>
+            </ul>
+        </div>
     </li>
 </template>
 
 <script>
+import IconDirectory from './Icons/IconDirectory.vue'
+import IconFile from './Icons/IconFile.vue'
+import IconLink from './Icons/IconLink.vue'
 export default {
     name: 'TreeItem',
-     props: {
-      item: Object
+    components: {IconDirectory, IconFile, IconLink},
+    props: {
+      item: Object,
+      currentPath: {
+          type: String,
+          default: ''
+      },
+      displayedPath: {
+          type: String,
+          default: ''
+      },
     },
     data() {
       return {
-        isOpen: false
-      };
+        isOpen: false,
+        isSelected: false,
+      }
     },
+    inject: ['getPath'],
     computed: {
       isFolder() {
         return this.item.type === 'directory'
+      },
+      icon() {
+        return `Icon${this.item.type[0].toUpperCase()}${this.item.type.slice(1)}`
+      },
+      itemPath() {
+          return this.currentPath + this.item.name + (this.isFolder ? '/' : ''); 
       }
     },
     methods: {
       toggle() {
         if (this.isFolder) {
-          this.isOpen = !this.isOpen;
+            this.isOpen = !this.isOpen;
         }
       },
-    }
+      passPath() {
+          this.$emit('path', this.itemPath)
+      },
+      handleSelection() {
+          this.passPath();
+          this.toggle();
+      }
+    },
 }
 </script>
 
-<style>
-
+<style scoped>
+.item__main {
+    display: flex;
+    align-items: center;
+    gap: 15px;
+    padding: 2px;
+}
+.item__main p {
+    margin: 0;
+}
+.item__main.selected {
+    background: #3b3b3b;
+}
+.item__main.selected p {
+    color: #fff;
+}
+.item__main.selected svg {
+    fill: #fff;
+}
 </style>
